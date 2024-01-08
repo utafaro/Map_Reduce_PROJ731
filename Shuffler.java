edafcba2fec817149745a08c70846d6db4c89409
loisblin <blinlois@gmail.com>
2024-01-08 12:16:34 +0100

    [Feature] focntion trier mot

--- Shuffler.java
+++ Shuffler.java
@@ -1,25 +1,46 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.*;
 
 public class Shuffler {
 
 
-    /*
-    public ArrayList<Map<String,Integer>> shuffle (Map<String,Integer> mapping, String worď){
+    public void trierMots(List<String> listeMots,List<String> liste_a_e,List<String> liste_f_l,List<String> liste_m_z) {
+        for(String mot:listeMots){
+            char premiereLettre = mot.charAt(0);
+            if (premiereLettre >= 'a' && premiereLettre <= 'e') {
+                liste_a_e.add(mot);}
+
+            if (premiereLettre >= 'f' && premiereLettre <= 'l') {
+                liste_f_l.add(mot);
+            }
+            else{
+                liste_m_z.add(mot);
+            }
+
+        }
+
+
+
+
+}}
+
+
+
+
+    /*public ArrayList<Map<String,Integer>> shuffle (Map<String,Integer> mapping, String word){
         ArrayList<Map<String,Integer>> result = new ArrayList<>();
         for (Map.Entry<String, Integer> entry : mapping.entrySet()){
-            if(worď == entry.getKey()){
+            if(word == entry.getKey()){
                 result.add();
             }
         }
     }
 
-     */
 
 
 
 
 
-}
+
+} */
 
